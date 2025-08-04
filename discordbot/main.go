3d4a7a241eb4e6a9a304870f67b325e3585a616b
Linus Jansson <linus.jansson2@uppsala.se>
2025-08-04 12:17:52 +0200

    feat: adding boilerplate discord bot code

--- discordbot/main.go
+++ discordbot/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+	session, _ := discordgo.New("Bot " + os.Getenv("dtoken"))
+	session.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages
+
+	err = session.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	log.Println("Bot is now running. Press CTRL+C to exit.")
+
+	defer session.Close()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Graceful shutdown")
 }
